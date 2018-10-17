3337f2fcac3eb15dddb7722719bdcfd02c52feac
Maulik Lalani <lalani.mau@gmail.com>
2018-10-17 19:00:47 +0530

    Add files via upload

---
+++ License_dapp/app/server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const MetaAuth = require('meta-auth');
+const Web3 = require('web3');
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+let contract_address = undefined
+
+// development
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+  contract_address = '0x9e17be3d288ba5365efed11c3fc20ff6318d90c5'
+  var web3 = new Web3(
+    new Web3.providers.HttpProvider('http://127.0.0.1:7545')
+  );
+}
+
+// production
+else {
+  contract_address = '0x9e17be3d288ba5365efed11c3fc20ff6318d90c5'
+  const mnemonic = process.env.MNEMONIC
+  const infura_api = process.env.INFURA_API 
+  var web3 = new Web3(
+    new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_api)
+  );
+}
+
+const contract_abi = require('./src/abi.js');
+const port = process.env.PORT || '3001'
+
+const app = express();
+const metaAuth = new MetaAuth({
+  banner: 'Token License Login'
+});
+
+var token = web3.eth.contract(contract_abi).at(contract_address);
+
+app.use('/', express.static('.'));
+
+app.get('/auth/:MetaAddress', metaAuth, (req, res) => {
+  // Request a message from the server
+  if (req.metaAuth && req.metaAuth.challenge) {
+    res.send(req.metaAuth.challenge)
+  }
+});
+
+app.get('/auth/:MetaMessage/:MetaSignature', metaAuth, (req, res) => {
+
+  if (req.metaAuth && req.metaAuth.recovered) {
+
+    // Check whether this user has a valid subscription in ERC721 token
+    token.balanceOf(req.metaAuth.recovered,
+      function (err, result) {
+        if (err) {
+          return console.error(err);;
+        }
+        else {
+          // Authentication is valid, assign JWT, etc.
+          console.log(result.c[0])
+         // if (result.c[0] > 0) res.send(req.metaAuth.recovered);
+         res.send(req.metaAuth.recovered);
+          // Authentication fail, no subscription token
+         // else res.status(400).send();
+        }
+    });
+
+  } else {
+    // Sig did not match, invalid authentication
+    res.status(400).send();
+  };
+});
+
+app.listen(port, () => {
+  console.log('Listening on port ' + port)
+})
---
+++ License_dapp/app/src/index.js
@@ -0,0 +1,151 @@
+import $ from 'jquery';
+var Web3 = require('web3')
+
+let contract_address = undefined
+let server_address = undefined
+let network_id = undefined
+
+// production
+if (window.location.hostname == 'licensetoken.atchai.com') {
+  contract_address = '0x9e17be3d288ba5365efed11c3fc20ff6318d90c5';  //ropsten
+  server_address = 'http://licensetoken.atchai.com'
+  network_id = 3 // ropsten - ethereum network ID
+}
+// dev
+else {
+  contract_address = '0x9e17be3d288ba5365efed11c3fc20ff6318d90c5';  //dev
+  server_address = 'http://localhost:3001'
+  network_id = "*" // ganache - ethereum network ID
+}
+
+const abi = require('./abi.js');
+const price = web3.toWei(0.1, 'ether');
+
+let challenge = null;
+let signature = null;
+
+// Check for Metamask and show/hide appropriate warnings.
+window.addEventListener('load', function() {
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if ((typeof web3 !== 'undefined') && (web3.givenProvider !== null)) {
+    var web3js = new Web3(web3.currentProvider);
+
+    // Checking if user is logged into an account
+    web3js.eth.getAccounts(function(err, accounts){
+        if (err != null) console.error("An error occurred: "+err);
+
+        // User is not logged into Metamask
+        else if (accounts.length == 0) {
+          $('#metamask-login').show();
+          console.log("User is not logged in to MetaMask");
+        }
+
+        // User is logged in to Metamask
+        else {
+          web3js.version.getNetwork((err, net_id) => {
+            console.log(net_id);
+            if (err != null) console.error("An error occurred: "+err);
+
+            // User is on the correct network
+            // Ropsten test network = 3, main net = 1
+            else if (net_id == network_id) {
+              console.log("User is logged in and on correct network");
+              $('#main-content').show();
+              startApp(web3js);
+            }
+
+            // User is not on the right network
+            else {
+              console.log("User is logged in and on WRONG network");
+              $('#metamask-network').show();
+            }
+        })
+      }
+  });
+
+  // User does not have Metamask / web3 provider
+  } else {
+    console.log('No web3? You should consider trying MetaMask!');
+    $('#metamask-install').show();
+  }
+})
+
+
+
+function startApp(web3js) {
+
+
+  var contract = web3js.eth.contract(abi).at(contract_address);
+
+  $('.buy').on('click', function () {
+    contract.purchaseSubscription({
+      'from':web3js.eth.accounts[0],
+      'value':price
+    },
+    function (err, transactionHash) {
+        console.log(err, transactionHash);
+    });
+  });
+
+  $('.balance').on('click', function () {
+    contract.balanceOf(web3js.eth.accounts[0],
+    function (err, res) {
+      if (err) {
+        return console.error(err);;
+      }
+      else {
+        $('#account_balance').text(res.c[0]);
+      }
+    });
+  });
+
+  $('.get').on('click', function () {
+    $('.challenge').empty();
+    $.get(server_address + '/auth/' + web3js.eth.accounts[0], (res) => {
+      challenge = res
+
+      res.forEach(line => {
+        $('.challenge').append(line.name);
+        $('.challenge').append('<br>');
+        $('.challenge').append(line.value);
+        $('.challenge').append('<br>');
+      })
+
+      const from = web3js.eth.accounts[0];
+
+      const params = [challenge, from];
+      const method = 'eth_signTypedData';
+
+      web3js.currentProvider.sendAsync({
+        method,
+        params,
+        from
+      }, async (err, result) => {
+        signature = result.result;
+        if (err) {
+          return console.error(err);
+        }
+        if (result.error) {
+          return console.error(result.error);
+        }
+        $('.signature').text(signature);
+      });
+    });
+
+  });
+
+  $('.verify').on('click', function() {
+
+    if (challenge[1].value) {
+      console.log("success",challenge[1].value);
+       $.get(server_address + '/auth/' + challenge[1].value + '/' + signature, (res) => {
+        if (res === web3js.eth.accounts[0]) {
+          $('.success').show();
+        } else {
+          $('.fail').show();
+        }
+      });
+    }
+    else $('.fail').show();
+  });
+}
